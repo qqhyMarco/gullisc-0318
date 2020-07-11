08c79744590474cd4a91c307168e30ee29ab537d
marco <2621760512123@qq.com>
2020-07-11 15:25:14 +0800

    '创建仓库'

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div >
+      {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+          msg:'noaho'
+        };
+    },
+ 
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
+
